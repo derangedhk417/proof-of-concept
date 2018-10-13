e5376fa101f1aa93a84ee7b20ad82ceb71158ed2
Adam Robinson <magnanimousllamacopter@gmail.com>
2018-10-13 09:33:04 -0400

    Updated mpi_controller.h with most recent

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,3 @@
 mpicc -o primary_slave.o primary_slave.c -lpthread -lrt
 gcc -shared -o testlib.so -fPIC test.c -lm -lpthread -lrt
-python run.py -np 8 -hosts 192.168.1.254,192.168.1.236 ./primary_slave.o
+python run.py -np 4 ./primary_slave.o
